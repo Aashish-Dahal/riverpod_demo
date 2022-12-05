b001611ebdad85f0c8965f31a4ed4b13f6d6cf16
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-05 22:02:48 +0545

    feat: provider state

--- lib/pages/provider_page.dart
+++ lib/pages/provider_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProviderPage extends StatelessWidget {
+final valueProvider = Provider<int>((ref) => 0);
+
+class ProviderPage extends ConsumerWidget {
   const ProviderPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Practice'),
+      ),
+      body: Center(
+          child: Text(
+        'Count: ${ref.watch(valueProvider)}',
+        style: Theme.of(context).textTheme.headline4,
+      )),
+    );
   }
 }
