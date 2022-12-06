2d66df9434125c648fa97741d608f2d18b97a465
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-06 13:55:51 +0545

    feat: cart state notifier

---
+++ lib/pages/cart_state_notifier.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo/model/product.dart';
+
+final cartStateNotifierProvider =
+    StateNotifierProvider<CartStateNotifier, List<Product>>(
+        (ref) => CartStateNotifier());
+
+class CartStateNotifier extends StateNotifier<List<Product>> {
+  CartStateNotifier() : super([]);
+
+  void addProduct(Product product) {
+    state = [...state, product];
+  }
+
+  void removeProduct(Product product) {
+    state = state.where((p) => p != product).toList();
+  }
+
+  void clearCart() {
+    state = [];
+  }
+}
