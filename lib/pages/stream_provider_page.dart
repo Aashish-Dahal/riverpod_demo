a52f987122173dca0fc9b021d17779e931826402
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-06 13:54:46 +0545

    feat: stream provider page

--- lib/pages/stream_provider_page.dart
+++ lib/pages/stream_provider_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo/services/stream_service.dart';
 
-class StreamProviderPage extends StatelessWidget {
+final streamValueProvider = StreamProvider((ref) {
+  final streamService = ref.watch(streamServiceProvider);
+  return streamService.getStream();
+});
+
+class StreamProviderPage extends ConsumerWidget {
   const StreamProviderPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final streamValue = ref.watch(streamValueProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream Provider'),
+      ),
+      body: streamValue.when(
+        data: (data) {
+          return Center(
+              child: Text(
+            data.toString(),
+            style: Theme.of(context).textTheme.headline4,
+          ));
+        },
+        error: (error, stackTrace) {
+          return Center(child: Text(error.toString()));
+        },
+        loading: () {
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
 }
